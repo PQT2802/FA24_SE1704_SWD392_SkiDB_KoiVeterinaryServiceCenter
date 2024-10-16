45396e66fcc3355440ce37bb693db40dc20c5b0a
Trần Gia Phúc <trangiaphuc362003181@gmail.com>
2024-10-16 07:22:25 +0700

    finish Appointment for FE

---
+++ KVSC.FrontEnd/KVSC.Infrastructure/DTOs/AppointmentForm/MakeAppointmentForServiceRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace KVSC.Infrastructure.DTOs;
+
+public class MakeAppointmentForServiceRequest
+{
+    public Guid CustomerId { get; set; }  // ID of the customer making the appointment
+    public Guid PetServiceId { get; set; }  // ID of the pet service for which the appointment is being made
+    public Guid? PetId { get; set; }  // Optional: ID of the pet associated with the appointment (if applicable)
+    public DateTime AppointmentDate { get; set; }  // The date and time of the appointment
+
+    public string Notes { get; set; }  // Optional: Any additional notes or requests for the appointment
+}
