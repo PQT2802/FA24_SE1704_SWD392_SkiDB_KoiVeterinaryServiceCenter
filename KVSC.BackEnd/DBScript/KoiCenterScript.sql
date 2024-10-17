4a4a7b83acf537d54bb088aafcfa849646dc8e87
phuctran362003 <trangiaphuc362003181@gmail.com>
2024-10-17 17:02:22 +0700

    update db script

--- KVSC.BackEnd/DBScript/KoiCenterScript.sql
+++ KVSC.BackEnd/DBScript/KoiCenterScript.sql
@@ -248,6 +248,42 @@ GO
 
 
 
+-- Insert sample data into PetHabitat table
+INSERT INTO [KoiCenterDB].[dbo].[PetHabitat]
+    ([Id], [HabitatType], [CreatedDate], [CreatedBy], [ModifiedDate], [ModifiedBy], [IsDeleted])
+VALUES
+    (NEWID(), 'Pond', GETDATE(), 'Admin', GETDATE(), 'Admin', 0),
+    (NEWID(), 'Aquarium', GETDATE(), 'Admin', GETDATE(), 'Admin', 0),
+    (NEWID(), 'Outdoor Water Garden', GETDATE(), 'Admin', GETDATE(), 'Admin', 0);
+
+-- Retrieve the generated GUIDs for PetHabitat
+SELECT [Id], [HabitatType] 
+FROM [KoiCenterDB].[dbo].[PetHabitat];
+
+
+
+	INSERT INTO [KoiCenterDB].[dbo].[PetType]
+    ([Id], [GeneralType], [SpecificType], [PetHabitatId], [CreatedDate], [CreatedBy], [ModifiedDate], [ModifiedBy], [IsDeleted])
+VALUES
+    (NEWID(), 'Fish', 'Koi Fish', '/* Pond Habitat Id here */', GETDATE(), 'Admin', GETDATE(), 'Admin', 0),
+    (NEWID(), 'Fish', 'Goldfish', '/* Aquarium Habitat Id here */', GETDATE(), 'Admin', GETDATE(), 'Admin', 0),
+    (NEWID(), 'Fish', 'Shubunkin', '/* Outdoor Water Garden Habitat Id here */', GETDATE(), 'Admin', GETDATE(), 'Admin', 0),
+    (NEWID(), 'Fish', 'Butterfly Koi', '/* Pond Habitat Id here */', GETDATE(), 'Admin', GETDATE(), 'Admin', 0),
+    (NEWID(), 'Fish', 'Comet Goldfish', '/* Aquarium Habitat Id here */', GETDATE(), 'Admin', GETDATE(), 'Admin', 0);
+
+
+INSERT INTO [KoiCenterDB].[dbo].[Pet]
+    ([Id], [Name], [Age], [Gender], [ImageUrl], [Color], [Length], [Weight], [LastHealthCheck], [HealthStatus], [OwnerId], [PetTypeId], [CreatedDate], [CreatedBy], [ModifiedDate], [ModifiedBy], [IsDeleted])
+VALUES
+    (NEWID(), 'Koi A', 2, 'Male', 'https://example.com/koi_a.jpg', 'Orange', 30.5, 2.1, '2023-10-01', 1, 'DD0E9F37-D587-401D-932E-7F098EB60B3E', NULL, GETDATE(), 'Admin', GETDATE(), 'Admin', 0),
+    (NEWID(), 'Koi B', 3, 'Female', 'https://example.com/koi_b.jpg', 'White', 35.0, 2.4, '2023-09-15', 2, '45A9DC1C-FB8A-4607-9A7E-D6B1359384D7', NULL, GETDATE(), 'Admin', GETDATE(), 'Admin', 0),
+    (NEWID(), 'Koi C', 1, 'Male', 'https://example.com/koi_c.jpg', 'Black', 25.2, 1.9, '2023-09-20', 1, 'BCA84E29-DE4D-475B-A3AD-A02E937EFA14', NULL, GETDATE(), 'Admin', GETDATE(), 'Admin', 0),
+    (NEWID(), 'Koi D', 4, 'Female', 'https://example.com/koi_d.jpg', 'Gold', 40.3, 3.0, '2023-08-25', 3, 'DD0E9F37-D587-401D-932E-7F098EB60B3E', NULL, GETDATE(), 'Admin', GETDATE(), 'Admin', 0),
+    (NEWID(), 'Koi E', 2, 'Male', 'https://example.com/koi_e.jpg', 'Red', 32.8, 2.2, '2023-10-05', 1, '45A9DC1C-FB8A-4607-9A7E-D6B1359384D7', NULL, GETDATE(), 'Admin', GETDATE(), 'Admin', 0);
+
+
+
+
 
 
 
