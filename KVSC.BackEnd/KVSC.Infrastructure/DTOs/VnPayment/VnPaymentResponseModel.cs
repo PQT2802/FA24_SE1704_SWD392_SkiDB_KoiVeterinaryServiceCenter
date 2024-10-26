9c1aea6ced34846f1a1f5acc797512315931fcf0
dotrong-code <dotrong159357@gmail.com>
2024-10-26 21:09:33 +0700

    setup_VnPay

---
+++ KVSC.BackEnd/KVSC.Infrastructure/DTOs/VnPayment/VnPaymentResponseModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KVSC.Infrastructure.DTOs.VnPayment
+{
+    public class VnPaymentResponseModel
+    {
+        public string PaymentMethod { get; set; }
+        public string OrderDescription { get; set; }
+        public string OrderId { get; set; }
+        public string PaymentId { get; set; }
+        public string TransactionId { get; set; }
+        public string Token { get; set; }
+        public string VnPayResponseCode { get; set; }
+    }
+}
