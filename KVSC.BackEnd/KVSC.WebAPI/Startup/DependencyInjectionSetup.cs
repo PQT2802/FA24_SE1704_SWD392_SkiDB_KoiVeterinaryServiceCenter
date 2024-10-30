707cbdf1475139e52cb57269f979322151c80ad1
Tran Gia Phuc <trangiaphuc362003181@gmail.com>
2024-10-31 01:29:02 +0700

    setup docker file

--- KVSC.BackEnd/KVSC.WebAPI/Startup/DependencyInjectionSetup.cs
+++ KVSC.BackEnd/KVSC.WebAPI/Startup/DependencyInjectionSetup.cs
@@ -2,60 +2,55 @@ using FirebaseAdmin;
 using FluentValidation;
 using Google.Apis.Auth.OAuth2;
 using Google.Cloud.Storage.V1;
+using KVSC.Application.Common.Validator.Appointment;
+using KVSC.Application.Common.Validator.ComboService;
 using KVSC.Application.Common.Validator.Pet;
+using KVSC.Application.Common.Validator.PetService;
+using KVSC.Application.Common.Validator.PetServiceCategory;
 using KVSC.Application.Common.Validator.Product;
 using KVSC.Application.Common.Validator.ProductCategory;
-using KVSC.Application.Common.Validator.Appointment;
-using KVSC.Application.Common.Validator.PetService;
+using KVSC.Application.Common.Validator.Rating;
+using KVSC.Application.Common.Validator.ServiceReport;
 using KVSC.Application.Common.Validator.User;
+using KVSC.Application.Common.Validator.VeterinarianSchedule;
 using KVSC.Application.Implement.Service;
 using KVSC.Application.Interface.ICommon;
 using KVSC.Application.Interface.IService;
-using KVSC.Application.KVSC.Application.Common.Validator.Abstract;
 using KVSC.Application.KVSC.Application.Common.Validator.User;
+using KVSC.Application.KVSC.Application.Implement.Service;
 using KVSC.Domain.Entities;
 using KVSC.Infrastructure.Common;
 using KVSC.Infrastructure.DTOs.Appointment.MakeAppointment;
+using KVSC.Infrastructure.DTOs.ComboService.UpdateComboService;
 using KVSC.Infrastructure.DTOs.Pet.AddComboService;
 using KVSC.Infrastructure.DTOs.Pet.AddPet;
-using KVSC.Infrastructure.DTOs.Product.AddProduct;
-using KVSC.Infrastructure.DTOs.Product.UpdateProduct;
-using KVSC.Infrastructure.DTOs.ProductCategory.AddProductCategory;
-using KVSC.Infrastructure.DTOs.ProductCategory.UpdateProductCategory;
-using KVSC.Infrastructure.DTOs.Pet.AddPetService;
 using KVSC.Infrastructure.DTOs.Pet.AddPetHabitat;
+using KVSC.Infrastructure.DTOs.Pet.AddPetService;
 using KVSC.Infrastructure.DTOs.Pet.AddPetType;
 using KVSC.Infrastructure.DTOs.Pet.UpdatePet;
 using KVSC.Infrastructure.DTOs.Pet.UpdatePetHabitat;
 using KVSC.Infrastructure.DTOs.Pet.UpdatePetType;
+using KVSC.Infrastructure.DTOs.PetService.UpdatePetService;
+using KVSC.Infrastructure.DTOs.PetServiceCategory.AddPetServiceCategroy;
+using KVSC.Infrastructure.DTOs.PetServiceCategory.UpdatePetServiceCategory;
+using KVSC.Infrastructure.DTOs.Product.AddProduct;
+using KVSC.Infrastructure.DTOs.Product.UpdateProduct;
+using KVSC.Infrastructure.DTOs.ProductCategory.AddProductCategory;
+using KVSC.Infrastructure.DTOs.ProductCategory.UpdateProductCategory;
+using KVSC.Infrastructure.DTOs.Rating.AddRating;
+using KVSC.Infrastructure.DTOs.Rating.UpdateRating;
+using KVSC.Infrastructure.DTOs.Schedule;
+using KVSC.Infrastructure.DTOs.ServiceReport.AddServiceReport;
+using KVSC.Infrastructure.DTOs.ServiceReport.UpdateServiceReport;
+using KVSC.Infrastructure.DTOs.User.AddUser;
 using KVSC.Infrastructure.DTOs.User.Register;
+using KVSC.Infrastructure.DTOs.User.UpdateUser;
 using KVSC.Infrastructure.Implement.Repositories;
 using KVSC.Infrastructure.Interface;
 using KVSC.Infrastructure.Interface.IRepositories;
 using KVSC.Infrastructure.KVSC.Infrastructure.Common;
 using KVSC.Infrastructure.KVSC.Infrastructure.DTOs.User.Login;
 using KVSC.Infrastructure.KVSC.Infrastructure.Implement.Repositories;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.OpenApi.Models;
-using KVSC.Application.KVSC.Application.Implement.Service;
-using KVSC.Infrastructure.DTOs.PetServiceCategory.AddPetServiceCategroy;
-using KVSC.Application.Common.Validator.PetServiceCategory;
-using KVSC.Application.Common.Validator.ComboService;
-using KVSC.Infrastructure.DTOs.PetService.UpdatePetService;
-using KVSC.Infrastructure.DTOs.PetServiceCategory.UpdatePetServiceCategory;
-using KVSC.Infrastructure.DTOs.ComboService.UpdateComboService;
-using KVSC.Application.Common.Validator.ServiceReport;
-using KVSC.Infrastructure.DTOs.ServiceReport.AddServiceReport;
-using KVSC.Infrastructure.DTOs.ServiceReport.UpdateServiceReport;
-
-using KVSC.Infrastructure.DTOs.User.UpdateUser;
-using KVSC.Infrastructure.DTOs.User.AddUser;
-
-using KVSC.Infrastructure.DTOs.Schedule;
-using KVSC.Application.Common.Validator.VeterinarianSchedule;
-using KVSC.Infrastructure.DTOs.Rating.AddRating;
-using KVSC.Infrastructure.DTOs.Rating.UpdateRating;
-using KVSC.Application.Common.Validator.Rating;
 
 namespace KVSC.WebAPI.Startup
 {
@@ -65,7 +60,7 @@ namespace KVSC.WebAPI.Startup
         public static IServiceCollection RegisterServices(this IServiceCollection services)
         {
             var credentialPath = Path.Combine(Directory.GetCurrentDirectory(), "Keys",
-                "koiveterinaryservicecent-925db-firebase-adminsdk-vus2r-0a84673789.json");
+                "koiveterinaryservicecent-925db-firebase-adminsdk-vus2r-7e9eaf02d5.json");
 
             try
             {
@@ -114,7 +109,7 @@ namespace KVSC.WebAPI.Startup
             services.AddTransient<IValidator<UpdateProductCategoryRequest>, UpdateProductCategoryValidator>();
 
             services.AddTransient<IValidator<AddPetServiceRequest>, AddPetServiceValidator>();
-            services.AddTransient<IValidator<UpdatePetServiceRequest>, UpdatePetServiceValidator>();    
+            services.AddTransient<IValidator<UpdatePetServiceRequest>, UpdatePetServiceValidator>();
 
             services.AddTransient<IValidator<AddComboServiceRequest>, AddComboServiceValidator>();
             services.AddTransient<IValidator<UpdateComboServiceRequest>, UpdateComboServiceValidator>();
@@ -164,7 +159,7 @@ namespace KVSC.WebAPI.Startup
             services.AddTransient<IServiceReportRepository, ServiceReportRepository>();
             services.AddTransient<IVeterinarianScheduleRepository, VeterinarianScheduleRepository>();
 
-            
+
             services.AddTransient<IRatingRepository, RatingRepository>();
 
 
