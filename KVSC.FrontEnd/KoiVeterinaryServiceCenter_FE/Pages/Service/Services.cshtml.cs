a45718a9fa3f224c7215e41cba936a48d2522774
Thang <pqt2802@gmail.com>
2024-11-03 22:12:47 +0700

    homepage_service

---
+++ KVSC.FrontEnd/KoiVeterinaryServiceCenter_FE/Pages/Service/Services.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KoiVeterinaryServiceCenter_FE.Pages.Service
+{
+    public class ServicesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ KVSC.FrontEnd/KoiVeterinaryServiceCenter_FE/Pages/Veterinarian/Veterinarian.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KoiVeterinaryServiceCenter_FE.Pages.Veterinarian
+{
+    public class VeterinarianModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
